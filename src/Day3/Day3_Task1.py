e7922ff68d8c7fd2d449488fa94df910b6e92edb
Raksha D K <rakshadk9@gmail.com>
2026-02-04 12:46:04 +0530

    Add files via upload

---
+++ src/Day3/Day3_Task1.py
@@ -0,0 +1,8 @@
+inventory=["Apples", "Bananas", "Carrots", "Dates"];
+print("Current Inventory:", inventory);
+inventory.append("Eggs");
+print("add inventory:",inventory);
+inventory.remove("Bananas");
+print("remove inventory:",inventory);
+inventory.sort();
+print("Final Updated inventory:",inventory);
