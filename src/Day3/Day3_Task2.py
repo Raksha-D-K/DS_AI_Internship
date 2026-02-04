d1da7cce02d5e67b56ddff155a4e4108b1afc242
Raksha D K <rakshadk9@gmail.com>
2026-02-04 12:46:50 +0530

    Add files via upload

---
+++ src/Day3/Day3_Task2.py
@@ -0,0 +1,12 @@
+temperatures=[22,24,25,28,30,29,27,26,24,22]
+print(temperatures)
+print("First index temperature:")
+print(temperatures[0])
+print("Last index temperature:")
+print(temperatures[-1])
+print("reading the 4th ,5th and 6th index temperatures:")
+print(temperatures[3:6])
+print("Last 3 Hours Temperatures")
+print(temperatures[-3:])
+
+
