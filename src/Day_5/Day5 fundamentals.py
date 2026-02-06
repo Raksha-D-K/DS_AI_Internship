bcf36ea175ab27c490e91b9866458aa985d85609
Raksha D K <rakshadk9@gmail.com>
2026-02-06 14:32:20 +0530

    Create Day5 fundamentals.py

---
+++ src/Day_5/Day5 fundamentals.py
@@ -0,0 +1,35 @@
+
+"""
+def greet():
+    print("Hello, Welcome to the internship program!")
+    greet()
+
+
+def add_numbers(a,b):
+    return a+b
+result=add_numbers(5,3)
+print(result)
+
+x=10
+def show_value():
+    print(x)
+show_value()
+print(x)
+
+
+food="Pizza"
+def junkfood():
+    food="Burger"
+    juice="Orange Juice"
+    print("food,is a junk food")
+    junkfood()
+    print("food,is a healthy food")
+   
+import math
+import random
+print(math.sqrt(16))
+print(random.randint(1,100))
+"""
+
+
+
