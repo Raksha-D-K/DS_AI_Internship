b84c91dcb47fd39f7645720d7c0da7bcbdb330d4
Raksha D K <rakshadk9@gmail.com>
2026-02-19 11:36:26 +0530

    Add files via upload

---
+++ src/Day16/Day16_task2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 19 11:28:27 2026
+
+@author: RAKSHA
+"""
+
+import pandas as pd
+import numpy as np
+
+np.random.seed(42)
+
+scores = np.random.normal(loc=70, scale=10, size=1000)
+
+scores = np.append(scores, [20, 120])
+
+data = pd.DataFrame({'Score': scores})
+
+mu = data['Score'].mean()
+sigma = data['Score'].std()
+
+print(f"Mean (μ): {mu:.2f}")
+print(f"Standard Deviation (σ): {sigma:.2f}")
+
+data['z_score'] = (data['Score'] - mu) / sigma
+
+outliers = data[np.abs(data['z_score']) > 3]
+
+print("\nStatistical Outliers (|Z| > 3):")
+print(outliers)
